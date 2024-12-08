74c499ca0faaa95b1ba2d97b85a1dd94969ca54a
JanS-12 <158617072+JanS-12@users.noreply.github.com>
2024-12-08 11:41:51 -0600

    Initial Commit, added basic AES encryption

--- index.py
+++ index.py
@@ -3,15 +3,21 @@
 # Student ID: 11536897
 # Course: CSCE 3550
 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends  import default_backend
+from cryptography.hazmat.primitives.padding import PKCS7
 from cryptography.hazmat.primitives import serialization
 from urllib.parse import urlparse, parse_qs
+from argon2 import PasswordHasher
 import datetime
 import sqlite3
 import base64
 import json
+import uuid
 import jwt
+import os
 
 hostName = "localhost"
 serverPort = 8080
@@ -19,36 +25,76 @@ serverPort = 8080
 # Database file
 db_file = "totally_not_my_privateKeys.db"
 
-# Connect to DB and create a table
+# Connect to DB 
 db = sqlite3.connect(db_file)
+
+# Create 'keys' table
 db.execute(
     '''CREATE TABLE IF NOT EXISTS keys(
             kid INTEGER PRIMARY KEY AUTOINCREMENT,
             key BLOB NOT NULL,
             exp INTEGER NOT NULL)''')
 
-# Re-using given code for key generation
-private_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-)
+# Create 'users' table
+db.execute(
+    '''CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        username TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        email TEXT UNIQUE,
+        date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        last_login TIMESTAMP)''')
 
-expired_key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048,
-)
+# Create 'auth_logs' table
+db.execute(
+    '''CREATE TABLE IF NOT EXISTS auth_logs(   
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        request_ip TEXT NOT NULL, 
+        request_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        user_id INTEGER, 
+        FOREIGN KEY(user_id) REFERENCES users(id)    
+    );''')
 
-pem = private_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.TraditionalOpenSSL,
-    encryption_algorithm=serialization.NoEncryption()
-)
+# Get environment variable
+def get_AES_key():
+    key = os.getenv("NOT_MY_KEY")
+    if not key:
+        raise ValueError("Environment variabel 'NOT_MY_KEY' is not set")
+    return bytes.fromhex(key)    
 
-expired_pem = expired_key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.TraditionalOpenSSL,
-    encryption_algorithm=serialization.NoEncryption()
-)
+# AES encrypting function
+def encrypt_AES(data: bytes) -> dict:
+    NOT_MY_KEY = get_AES_key()     # NOT_MY_KEY
+    iv = os.urandom(16)     # Initialization Vector (16-byte) 
+    cipher = Cipher(algorithms.AES(NOT_MY_KEY), modes.CBC(iv), backend=default_backend())
+    
+    # Pad data
+    padder = PKCS7(128).padder() # 128-bit block size for AES
+    padded_data = padder.update(data.encode()) + padder.finalize()
+    
+    # Encrypt the padded data
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(padded_data) + encryptor.finalize()   
+    
+    return {"ciphertext": ciphertext, "iv": iv}
+
+# AES decrypting function
+def decrypt_AES(data: dict) -> bytes:
+    NOT_MY_KEY = get_AES_key()         # NOT_MY_KEY
+    iv = data["iv"]             # Initialization Vector (16-byte)
+    ciphertext = data["ciphertext"]         # Encrypted Data
+    
+    cipher = Cipher(algorithms.AES(NOT_MY_KEY), modes.CBC(iv), backend=default_backend)
+    
+    # Decrypt ciphertext
+    decryptor = cipher.decryptor()
+    padded_data = decryptor.update(ciphertext) + decryptor.finalize()
+    
+    # Remove Padding
+    unpadder = PKCS7(128).unpadder()
+    data = unpadder.update(padded_data) + unpadder.finalize()
+    
+    return data.decode()
 
 # Re-using code for integer conversion to Base64URL
 def int_to_base64(value):
@@ -61,8 +107,18 @@ def int_to_base64(value):
     encoded = base64.urlsafe_b64encode(value_bytes).rstrip(b'=')
     return encoded.decode('utf-8')
 
-numbers = private_key.private_numbers()
-
+# Serialize key to PEM format
+def serialize_key(key):
+    return key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+        )
+    
+# Deserialize key from PEM format    
+def deserialize_pem(pem):
+    return serialization.load_pem_private_key(pem, password=None) 
+   
 # Save the keys to DB
 def store_key(k_pem, exp):
     with db:
@@ -70,21 +126,40 @@ def store_key(k_pem, exp):
         
 # Function to get a key from the database (expired or valid)
 def get_key(expired=False):
+    current_time = int(datetime.datetime.now(datetime.UTC).timestamp)
+    
     with db:
         if expired:
-            result = db.execute("SELECT key, exp FROM keys WHERE exp <= ?", (int(datetime.datetime.now(datetime.UTC).timestamp()),))
+            result = db.execute("SELECT key, exp FROM keys WHERE exp <= ? ORDER BY exp DESC LIMIT 1", (current_time,))
         else:
-            result = db.execute("SELECT key, exp FROM keys WHERE exp > ?", (int(datetime.datetime.now(datetime.UTC).timestamp()),))
-             
+            result = db.execute("SELECT key, exp FROM keys WHERE exp > ? ORDER BY exp ASC LIMIT 1", (current_time,))
         row = result.fetchone()
-         
-        return row
+        
+    if row:             # Return kid with the corresponding private key 
+        return row[0], deserialize_pem(decrypt_AES(row[1]))      
+    return None, None
     
-# Store valid key
-store_key(pem.decode(), int((datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)).timestamp()))  
+# Re-using given code for key generation
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+expired_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 
-# Store expired key (by 5 hours)
-store_key(expired_pem.decode(), int((datetime.datetime.now(datetime.UTC) - datetime.timedelta(hours=5)).timestamp()))    
+# Serialize keys
+private_pem = serialize_key(private_key)
+expired_pem = serialize_key(expired_key)
+   
+# Encrypt serialized keys, and respective IVs
+encrypted_private_key, IV_V = encrypt_AES(private_pem.encode('utf-8'))
+encrypted_expired_key, IV_E = encrypt_AES(expired_pem.encode('utf-8'))   
+
+current_time = int(datetime.datetime.now(datetime.UTC))
+   
+# Store valid key (expiration 1 hour from now)
+store_key(encrypted_private_key.decode(), current_time + datetime.timedelta(hours=1).timestamp())  
+
+# Store expired key (expired by 5 hours)
+store_key(encrypted_expired_key.decode(), current_time - datetime.timedelta(hours=5).timestamp())    
+
+db.commit()
 
 # HTTP Server logic remains unchanged
 class MyServer(BaseHTTPRequestHandler):
@@ -104,6 +179,11 @@ class MyServer(BaseHTTPRequestHandler):
         if parsed_path.path == "/auth":
             exp = 'expired' in params
             row = get_key(exp)            
+            
+            # For each 'auth' request, log the following into the 'auth_logs' table:
+                # Request IP Address
+                # Timestamp of the request
+                # User ID of the username
 
             if row:
                 k_pem = row[0]      # Index 0 is pem, Index 1 is exp
@@ -125,6 +205,7 @@ class MyServer(BaseHTTPRequestHandler):
                 self.wfile.write(bytes(encoded_jwt, "utf-8"))
             else:
                 self.not_supported_methods() # Key not found
+        #elif parsed_path.path == "/register":
         return
 
     #GET Request (/.well-known/jwks.json)
